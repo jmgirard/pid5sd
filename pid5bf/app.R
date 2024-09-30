6f15f486f7f6a582bbbb26ef5c75375d8dc8307d
Jeffrey Girard <me@jmgirard.com>
2024-09-30 15:08:05 -0500

    add pid5bf demo

---
+++ pid5bf/app.R
@@ -0,0 +1,42 @@
+library(surveydown)
+library(hitop)
+
+# Database setup
+db <- sd_database(
+  host   = "aws-0-us-west-1.pooler.supabase.com",
+  dbname = "postgres",
+  port   = "6543",
+  user   = "postgres.outtobdgbkorhxfkpijc",
+  table  = "newpid",
+  ignore = FALSE
+)
+
+
+# Setup server
+server <- function(input, output, session) {
+
+  survey_data <- sd_get_data(db, refresh_interval = 10)
+  
+  output$my_table <- shiny::renderTable({
+    df <- survey_data()
+    df2 <- df[nrow(df), sprintf("bf_%03d", 1:25)]
+    hitop::score_pid5bf(df2)
+  })
+  
+  # Define any conditional skip logic here (skip to page if a condition is true)
+  #sd_skip_if()
+
+  # Define any conditional display logic here (show a question if a condition is true)
+  #sd_show_if()
+
+  # Database designation and other settings
+  sd_server(
+    db = db,
+    use_html = TRUE,
+    admin_page = TRUE
+  )
+
+}
+
+# Initiate shiny
+shiny::shinyApp(ui = sd_ui(), server = server)
